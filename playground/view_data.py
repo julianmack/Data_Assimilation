56cb98da4018c686bd608bfa9ca43bc55ab85949
Julian Mack <julianmack@hotmail.co.uk>
2019-04-09 11:15:49 +0100

    added Dask arrays to playground code

--- playground/view_data.py
+++ playground/view_data.py
@@ -3,6 +3,8 @@
 import numpy as np
 import pandas as pd
 
+import dask.array as da
+
 import sys
 import os
 fluidity_fp  = '/mnt/c/Users/julia/fluidity/fluidity-master/python'
@@ -13,15 +15,17 @@ sys.path.append(DA_project_fp)
 import vtktools
 ug1=vtktools.vtu(DA_project_fp + '/data/LSBU_c_0.vtu')
 ug2=vtktools.vtu(DA_project_fp + '/data/LSBU_0.vtu')
+
 print(ug1.GetFieldNames())
 print(ug2.GetFieldNames())
 
 #read the values of the tracers and copy in a vector named p
-p=ug2.GetVectorField('Velocity')
+p = ug2.GetVectorField('Velocity')
 
-n=len(p)
+p = da.from_array(p, chunks = [5000, 3])
+n = len(p)
 print(n)
 
 
-Background = np.matmul(p, p.T)
+Background = da.matmul(p, p.T)
 print(Background.shape)
