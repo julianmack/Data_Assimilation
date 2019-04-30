13753c70946cff5fd9e28160230a6de25ac373e7
Julian Mack <julianmack@hotmail.co.uk>
2019-04-30 14:55:03 +0100

    renamed files

--- utils.py
+++ utils.py
@@ -13,3 +13,18 @@ def set_seeds(seed = SEED):
     np.random.seed(SEED)
     random.seed(SEED)
     torch.cuda.manual_seed(SEED)
+
+
+class ML_utils():
+    """Class to hold ML helper functions"""
+
+    def __init__():
+        pass
+
+    @staticmethod
+    def training_loop(model, optimizer, loss, train_loader, print_every=1, epochs, device):
+        """Runs a torch model training loop"""
+        model.train()
+        for epoch in range(epoch):
+            for batch_idx, data in enumerate(train_loader):
+                
