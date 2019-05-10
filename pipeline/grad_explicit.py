3f14c85853c9886fc08579625a72bd04d0c0abb1
Julian Mack <julianmack@hotmail.co.uk>
2019-05-10 15:04:40 +0100

    explicit grad working for toy case

--- pipeline/grad_explicit.py
+++ pipeline/grad_explicit.py
@@ -1,9 +1,12 @@
 import torch.nn as nn
+import torch.nn.functional as F
+
 import torch
 import numpy as np
 from utils import ML_utils as ML
 import utils
 
+import time
 class ToyNet(nn.Module):
     """Creates simple toy network with one fc hidden layer"""
     def __init__(self, inn, hid, out):
@@ -13,7 +16,7 @@ class ToyNet(nn.Module):
         self.sizes = [inn, hid, out]
 
     def forward(self, x):
-        h = torch.F.ReLU(self.fc1(x))
+        h = F.relu(self.fc1(x))
         h = self.fc2(h)
         return h
 
@@ -22,8 +25,8 @@ class ToyNet(nn.Module):
         """
         [inn, hid, out] = self.sizes
         #weights
-        W_a = torch.rand((hid, inn), requires_grad=True)
-        W_b = torch.rand((out, hid), requires_grad=True)
+        W_a = torch.rand((hid, inn), requires_grad=True) - 0.5
+        W_b = torch.rand((out, hid), requires_grad=True) - 0.5
 
         #biases
         b_a = torch.rand((hid,), requires_grad=True)
@@ -35,21 +38,59 @@ class ToyNet(nn.Module):
         self.fc1.bias = nn.Parameter(b_a)
         self.fc2.bias = nn.Parameter(b_b)
 
+    def jac_explicit(self, x):
+        """Generate explicit gradient
+        (from hand calculated expression)"""
+
+        W_a = self.fc1.weight
+        W_b = self.fc2.weight
+        b_a = self.fc1.bias
+        b_b = self.fc2.bias
+
+        z_1 = (x @ W_a.t()) + b_a
+        #A = torch.sign(z_1).unsqueeze(2)
+        A = (z_1 > 0).unsqueeze(2).type(torch.FloatTensor)
+
+        B = W_b.t().expand((z_1.shape[0], -1, -1))
 
 
+        first = torch.mul(A, B)
+        first = torch.transpose(first, 1, 2)
+
+        jac = first @ W_a
+
+        jac = torch.transpose(jac, 1, 2)
+
+        return jac
+        # print(A)
+        # print(B)
+        # print("A.shape:", A.shape, )
+        # print("B.shape:", B.shape, )
+        # print(first.shape)
+        # print(jac.shape)
 
 if __name__ == "__main__":
-    INPUT = 3
-    OUT = 16
-    HIDDEN = 8
-    Batch_sz = 1
+    INPUT = 40
+    HIDDEN = 50
+    OUT = 60
+    Batch_sz = 4
     utils.set_seeds()
 
     model = ToyNet(INPUT, HIDDEN, OUT)
     model.gen_rand_weights()
 
-    input = torch.rand((Batch_sz, INPUT))
+    input = torch.rand((Batch_sz, INPUT), requires_grad=True)
     output = model(input)
 
     jac_true = ML.jacobian_slow_torch(input, output)
-    print(jac_true)
+
+    jac_expl = model.jac_explicit(input)
+
+    # print("True jac shape", jac_true.shape)
+    # print("jac explicit shape", jac_expl.shape)
+    #
+    # print(jac_true)
+    # print(jac_expl)
+
+    assert torch.allclose(jac_true, jac_expl), "Two jacobians are not equal"
+    print("SUCCESS")
