ef87e30a2752ab89fb7de2e688dfacb76ed21914
Julian Mack <julianmack@hotmail.co.uk>
2019-05-17 17:48:59 +0100

    Placed diagnostics into pipeline

---
+++ pipeline/diagnostics/AE_vs_SVD.py
@@ -0,0 +1,11 @@
+import pipeline
+from pipeline import DAPipeline
+class AE_TruncSVD():
+    def __init__(self, AEsettings=None):
+        self.SVDsettings = pipeline.config.Config()
+        self.AEsettings = pipeline.config.ToyAEConfig if AEsettings is None else AEsettings
+
+    def compare(self):
+        DA = pipeline.DAPipeline()
+
+        w_opt
