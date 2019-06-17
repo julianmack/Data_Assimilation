e7da5ded98e46be677437cc2945ddc67eee0c6c0
Julian Mack <julianmack@hotmail.co.uk>
2019-06-17 12:27:48 +0100

    Added test for multiple hiddne layer AE

--- tests/test_ToyAE_jac_explicit.py
+++ tests/test_ToyAE_jac_explicit.py
@@ -17,3 +17,18 @@ def test_jac_explicit_correct_single_hid_layer():
     jac_expl = model.jac_explicit(input)
 
     assert torch.allclose(jac_true, jac_expl, rtol=1e-02), "Two jacobians are not equal"
+
+def test_jac_explicit_correct_mult_hid_layer():
+    INPUT = 32
+    OUT = 4
+    HIDDEN = [128, 128, 64]
+    Batch_sz = 64
+    input = torch.rand((Batch_sz, INPUT), requires_grad=True)
+    model = ToyAE(INPUT, HIDDEN, OUT)
+
+    output = model.decode(input)
+
+    jac_true = ML.jacobian_slow_torch(input, output)
+    jac_expl = model.jac_explicit(input)
+
+    assert torch.allclose(jac_true, jac_expl, rtol=1e-02), "Two jacobians are not equal"
