c68c4ca9481b9f0ec7a964fb126bb26e050da192
Julian Mack <julianmack@hotmail.co.uk>
2019-06-17 12:23:34 +0100

    Updated setup.py file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='DApipeline',
     version='0.1',
-    description='pipeline for AE-based Variational Data Assimilation'
+    description='pipeline for AE-based Variational Data Assimilation',
     packages=find_packages(exclude=[]),
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )
