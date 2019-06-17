2a81d286d06d09d74ae509124689e91ffe51b259
Julian Mack <julianmack@hotmail.co.uk>
2019-06-17 11:01:58 +0100

    Split testing and diagnosis of explicit gradient fn in ToyAE

---
+++ tests/test_ToyAE_jac_explicit.py
@@ -0,0 +1,19 @@
+import torch
+from pipeline.utils import ML_utils as ML
+
+from pipeline.AutoEncoders import ToyAE
+
+def test_jac_explicit_correct_single_hid_layer():
+    INPUT = 32
+    OUT = 4
+    HIDDEN = 128
+    Batch_sz = 64
+    input = torch.rand((Batch_sz, INPUT), requires_grad=True)
+    model = ToyAE(INPUT, HIDDEN, OUT)
+
+    output = model.decode(input)
+
+    jac_true = ML.jacobian_slow_torch(input, output)
+    jac_expl = model.jac_explicit(input)
+
+    assert torch.allclose(jac_true, jac_expl, rtol=1e-02), "Two jacobians are not equal"
