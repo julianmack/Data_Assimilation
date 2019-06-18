e6c7e975c802aafd33befa4018b18675a85b8a9f
Julian Mack <julianmack@hotmail.co.uk>
2019-06-18 22:38:12 +0100

    Added calculation of total number of points

--- main_interpolate.py
+++ main_interpolate.py
@@ -1,4 +1,4 @@
-from pipeline import DataAssimilation, config, utils
+from pipeline import config, utils
 import numpy as np
 import vtk.util.numpy_support as ns
 
@@ -6,17 +6,22 @@ def main():
     fluidity = utils.FluidityUtils()
 
     settings = config.Config()
-    DA = DataAssimilation.DAPipeline(settings)
 
-    fps = DA.get_sorted_fps_U(settings.DATA_FP)
+    fps = utils.DataLoader.get_sorted_fps_U(settings.DATA_FP)
 
     #fp_alt = settings.HOME_DIR + "data/" + "DA_MAE.vtu"
 
-    in_3d = fluidity.get_3D_grid(fps[1], settings.FIELD_NAME, save_newgrid_fp = "data/interpolate2", ret_torch = False)
+    in_3d = fluidity.get_3D_grid(fps[2], settings.FIELD_NAME,
+                        save_newgrid_fp = "data/interpolate2",
+                        ret_torch = False, factor_inc = 2.43)
 
     #vtk_file = ns.numpy_to_vtk(in_3d, 1)
 
     print(in_3d.shape)
+    prod = 1
+    for x in in_3d.shape:
+        prod = prod * x
+    print(prod)
 
 if __name__ == "__main__":
     main()
