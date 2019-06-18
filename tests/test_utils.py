04e6e99918b740afb9809b871629a1daec869d9b
Julian Mack <julianmack@hotmail.co.uk>
2019-06-18 10:43:27 +0100

    Added explicit tests for jacobian_slow_torch

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,7 @@ from pipeline import config
 import os
 import pytest
 import numpy as np
+import torch
 
 class TestSeed():
     def test_set_seeds_normal(self):
@@ -21,3 +22,33 @@ class TestSeed():
             del env["SEED"]
         with pytest.raises(NameError):
             utils.set_seeds()
+
+class TestJacSlow():
+    def test_jac_slow_no_batch(self):
+        input_size = 3
+        hidden = 5
+        latent_dim = 2
+
+        x = torch.rand((latent_dim,), requires_grad=True)
+        W = torch.rand((hidden, latent_dim))
+        b = torch.rand((hidden, ))
+        y = W @ x + b
+
+        grad = utils.ML_utils.jacobian_slow_torch(x, y)
+
+        assert np.allclose(W, grad)
+
+    def test_jac_slow_batch(self):
+        input_size = 3
+        hidden = 5
+        latent_dim = 2
+        batch_sz = 4
+
+        X = torch.rand((batch_sz, latent_dim,), requires_grad=True)
+        W = torch.rand((hidden, latent_dim))
+        b = torch.rand((hidden, ))
+        y = X @ W.t() + b
+        W_stacked = W.expand((batch_sz, -1, -1))
+        grad = utils.ML_utils.jacobian_slow_torch(X, y)
+
+        assert np.allclose(W_stacked, grad)
