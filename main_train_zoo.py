b74e9eddf5df5e20b9f42c9ce95f1804298e2a6c
julianmack <julianmack@hotmail.co.uk>
2019-07-01 18:33:47 +0100

    Removed just-init flag

--- main_train_zoo.py
+++ main_train_zoo.py
@@ -2,7 +2,7 @@
 from pipeline import config
 from pipeline import TrainAE
 from pipeline.AEs.CAE_configs import ARCHITECTURES as architectures
-TEST_INIT_ONLY = True
+TEST_INIT_ONLY = False
 
 def main():
     activations = ["lrelu", "relu"] # first experiments showed "lrelu" much better than "relu"
