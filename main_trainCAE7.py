8b9b9e41e5009e676eb5dc519418b111bd64f87f
julianmack <julianmack@hotmail.co.uk>
2019-07-22 10:51:47 +0100

    CAE7 training loop w. exception handling

--- main_trainCAE7.py
+++ main_trainCAE7.py
@@ -6,8 +6,8 @@ from pipeline.settings.CAE7 import CAE7
 
 def main():
     mults = [1, 2, 4, 8]
-    latent_mults = [2]
-    BNs = [True, False]
+    latent_mults = [2, 4]
+    BNs = [ False]
     for latent in latent_mults:
         for mult in mults:
             for BN in BNs:
@@ -25,19 +25,26 @@ def main():
                 else:
                     batch = "NBN"
                 expdir = "experiments/train_DA_{}/{}-l{}{}".format(settings.FIELD_NAME, mult, latent, batch)
-                epochs = 200
+                epochs = 300
                 calc_DA_MAE = True
                 num_epochs_cv = 20
                 small_debug = False
                 print_every = 5
                 test_every = 5
                 lr = 0.001
-                trainer = TrainAE(settings, expdir, calc_DA_MAE)
-                num_encode = len(trainer.model.layers_encode)
+                try:
+                    trainer = TrainAE(settings, expdir, calc_DA_MAE)
 
-                model = trainer.train(epochs, learning_rate=lr, test_every=test_every, num_epochs_cv=num_epochs_cv,
-                                        print_every=print_every, small_debug=small_debug)
+                    model = trainer.train(epochs, learning_rate=lr, test_every=test_every, num_epochs_cv=num_epochs_cv,
+                                            print_every=print_every, small_debug=small_debug)
+                except:
+                    try:
+                        trainer = TrainAE(settings, expdir, calc_DA_MAE, batch_sz=8)
 
+                        model = trainer.train(epochs, learning_rate=lr, test_every=test_every, num_epochs_cv=num_epochs_cv,
+                                                print_every=print_every, small_debug=small_debug)
+                    except:
+                        pass
 
 
 if __name__ == "__main__":
