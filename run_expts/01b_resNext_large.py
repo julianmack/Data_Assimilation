da47d21752c40a4eebb60654977e48ec8ac4c326
julianmack <julianmack@hotmail.co.uk>
2019-08-11 00:06:43 +0100

    Added experiments to a) test a larger res module with new setup and b) run resNeXt expt on two separate nodes

---
+++ run_expts/01b_resNext_large.py
@@ -0,0 +1,52 @@
+"""
+Check that you can get a resNext system to train
+
+"""
+
+from pipeline.settings.models_.resNeXt import ResNeXt
+
+
+from pipeline import TrainAE, ML_utils, GetData, SplitData
+from pipeline.VarDA.batch_DA import BatchDA
+
+
+#global variables for DA and training:
+EPOCHS = 100
+SMALL_DEBUG_DOM = False #False #False #For training
+calc_DA_MAE = True
+num_epochs_cv = 0
+LR = 0.0003
+print_every = 5
+test_every = 5
+exp_base = "experiments/train/01b/"
+GPU_DEVICE = 0
+def main():
+    layer = 6
+    cardinality = 4
+    print("Layers", layer)
+    print("Cardinality", cardinality)
+
+    kwargs = {"layers": layer, "cardinality": cardinality}
+
+    settings = ResNeXt(**kwargs)
+    settings.AUGMENTATION = True
+    settings.DEBUG = False
+    settings.GPU_DEVICE = GPU_DEVICE
+    settings.SEED = 19
+    settings.export_env_vars()
+
+    expdir = exp_base
+
+    trainer = TrainAE(settings, expdir, calc_DA_MAE)
+    expdir = trainer.expdir #get full path
+
+
+    model = trainer.train(EPOCHS, test_every=test_every, num_epochs_cv=num_epochs_cv,
+                            learning_rate = LR, print_every=print_every, small_debug=SMALL_DEBUG_DOM)
+
+
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ run_expts/01c_resNeXt.py
@@ -0,0 +1,58 @@
+"""
+Train all resNext - split over two nodes.
+lower learning rate to 0.0002 (struggling to learn at low values)
+"""
+from pipeline.settings.models_.resNeXt import ResNeXt
+
+
+from pipeline import TrainAE, ML_utils, GetData, SplitData
+from pipeline.VarDA.batch_DA import BatchDA
+
+import shutil
+
+#global variables for DA and training:
+EPOCHS = 150
+SMALL_DEBUG_DOM = False #For training
+calc_DA_MAE = True
+num_epochs_cv = 0
+LR = 0.0002
+print_every = 10
+test_every = 10
+PARAM_IDX = 0
+
+exp_base = "experiments/train/01_resNeXt_3/{}/".format(PARAM_IDX)
+def main():
+    #of form (layers, cardinality)
+    param_vals = [(3, 1), (3, 8), (3, 32), (27, 32)]
+    param_vals_2 = [(9, 1), (9, 8), (9, 32), (27, 1), (27, 8),]
+    param_options = [param_vals, param_vals_2]
+
+    idx = 0
+    params = param_options[PARAM_IDX]
+    for param in params:
+        layer, cardinality = param
+        print("Layers", layer)
+        print("Cardinality", cardinality)
+
+        kwargs = {"layers": layer, "cardinality": cardinality}
+
+        settings = ResNeXt(**kwargs)
+        settings.AUGMENTATION = True
+        settings.DEBUG = False
+        expdir = exp_base + str(idx) + "/"
+
+
+        trainer = TrainAE(settings, expdir, calc_DA_MAE)
+        expdir = trainer.expdir #get full path
+
+
+        model = trainer.train(EPOCHS, test_every=test_every, num_epochs_cv=num_epochs_cv,
+                                learning_rate = LR, print_every=print_every, small_debug=SMALL_DEBUG_DOM)
+
+        idx += 1
+
+
+
+if __name__ == "__main__":
+    main()
+
