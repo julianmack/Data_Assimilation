f701303fbfb5d0520e45696412bf79bf6be3bb30
julianmack <julianmack@hotmail.co.uk>
2019-08-11 00:05:06 +0100

    Updated main scripts

--- main_vardaSVD.py
+++ main_vardaSVD.py
@@ -7,16 +7,17 @@ def main():
 
     settings = config.Config()
 
-    settings.NORMALIZE = False
-    settings.UNDO_NORMALIZE = False
-    settings.SHUFFLE_DATA = False
-    settings.OBS_VARIANCE = 10
+    settings.NORMALIZE = True
+    settings.UNDO_NORMALIZE = True
+    settings.SHUFFLE_DATA = True
+    settings.OBS_VARIANCE = 0.05
     settings.SAVE = True
     settings.FORCE_GEN_X = False
     settings.AZURE_DOWNLOAD = True
 
     settings.THREE_DIM = True
-    
+    settings.set_n((91, 85, 32))
+
     DA = DAPipeline(settings)
 
     print(settings.COMPRESSION_METHOD)
